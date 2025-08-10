85a725c6bce923edbdf4adf776bc588dbbb2baeb
FifthWit <fifthwitbusiness@gmail.com>
2025-08-09 22:34:00 -0500

    w-fit for taskbar as it felt too empty

--- src/components/taskbar.svelte
+++ src/components/taskbar.svelte
@@ -5,7 +5,7 @@
 
 </script>
 
-<div class="px-2 flex flex-row items-center justify-between fixed top-0 left-1/2 transform -translate-x-1/2 w-128 h-8 bg-neutral-800 border border-t-0 border-neutral-700 rounded-b-xl">
+<div class="px-2 flex flex-row items-center justify-between fixed top-0 left-1/2 transform -translate-x-1/2 w-fit h-8 bg-neutral-800 border border-t-0 border-neutral-700 rounded-b-xl">
     <Rss />
     <Chatbot />
     <Mail />
